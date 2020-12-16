ece2211ee2ba9e9b4e398b2d8f050a717589600c
Carlos Bueno <charlie@Carloss-MacBook-Air.local>
2020-12-16 12:30:25 -0500

    adición de políticas CORS y función para autenticar inicio de sesión de usuario

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ from fastapi import HTTPException
 
 api = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+"http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+"http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+CORSMiddleware, allow_origins=origins,
+allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 #Consultar usuario
 @api.get("/user/{username}")
 async def get_user_information(username: str):
     user_in_db = get_user(username)
     if user_in_db == None:
-        raise HTTPException(status_code = 404, detail = "El usuario no existe.")
+        return{"message": "El usuario no existe."}
     if not user_in_db.active:
         return {"message": "El usuario ya no se encuentra activo."}
     user_out = UserOut(**user_in_db.dict())
@@ -32,3 +42,16 @@ async def unsubscribe_user(user: UserIn):
         return {"message": "El usuario ha sido dado de baja."}
     else:
         return {"message": "El usuario no existe o la contraseña es incorrecta"}
+
+#Autenticación del usuario
+@api.post("/user/auth/")
+async def auth_user(user_in: UserIn):
+    user_in_db = get_user(user_in.username)
+    if user_in_db == None:
+        return {"message": "El usuario no existe"}
+    if user_in_db.active == False:
+        return {"message": "El usuario se encuentra inactivo"}
+    if user_in_db.password != user_in.password:
+        return {"message": "La contraseña es incorrecta"}
+    else:
+        return UserOut(**user_in_db.dict())
